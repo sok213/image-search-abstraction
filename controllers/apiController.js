c288987a8ca16e6c3667e4d97a72e8937d71d1ca
sophanarith sok <sophanarithsok@gmail.com>
2017-01-22 14:57:07 -0800

    implement formatting of json data improve JSON response to client

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,34 +1,45 @@
 const config  = require('../config'),
   searchQuery = require('../model/imageResultModel'),
   request     = require('request');
-  
-function searchObj(url, tags, pageURL) {
-  this.url     = url;
-  this.tags    = tags;
-  this.pageURL = pageURL;
-}
 
-module.exports = function(app) {
-  app.get('/api/search/:term', function(req, res) {
+module.exports = app => {
+  app.get('/api/search/:term', (req, res) => {
     // API request to get search term data from pixabay.
     request(`https://pixabay.com/api/?key=${ config.apiKEY() }` + 
-      `&q=${ req.params.term }&pretty=true&per_page=3`, 
+      `&q=${ req.params.term }&pretty=true&per_page=10`, 
       // Callback function for HTTP request.
-      function(err, response, data) {
+      (err, response, data) => {
         // If no error, send JSON data as an HTTP reponse.
         if (!err && response.statusCode == 200) {
           console.log(`Search for ${ req.params.term } was successful!`);
           
+          // Format object to be sent as a response.
+          let searchResults = {
+            totalHits: JSON.parse(data).totalHits,
+            hits: [],
+            total: JSON.parse(data).total
+          };
+          
           // Create newSearch object with values retrieved from URL parameter
-          var newSearch = searchQuery({
+          let newSearch = searchQuery({
             url: req.params.term,
             timeSearched: new Date().toISOString()
           });
-          console.log("Length: " + data.length);
+          
+          // Format and push all results into searchResults object.
+          JSON.parse(data).hits.forEach(hit => {
+            searchResults.hits.push(
+              {
+                url: hit.webformatURL, 
+                tag: hit.tags,
+                pageURL: hit.pageURL
+              }
+            );
+          });
           
           // Save object to database and send JSON data as response to client.
           newSearch.save();
-          res.send(data);
+          res.send(searchResults);
         }
       }
     );
